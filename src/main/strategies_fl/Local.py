b8585b069f8a88997c883d8d480838b8e5d4d881
luannt0801 <ntl080102@gmail.com>
2024-11-05 23:57:22 +0700

    _local start

---
+++ src/main/strategies_fl/Local.py
@@ -0,0 +1,22 @@
+from src.add_config import *
+
+def client():
+    
+    pass
+
+def local_running():
+    """
+    Server call trainning:
+        Input:
+            n rounds | Strategy | num_clients | 
+
+    Client trainning:
+        Input:
+            model | batch_size | lr | drop_last | num_epochs
+            dataset | num_classes | partitition | data_volume_each_client | beta |rho
+
+    """
+
+
+if __name__ == "__main__":
+    local_running()
